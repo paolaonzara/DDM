99b95ce8c03f2e5eb1be66421d0640b2ca317bd1
Paola Onório Zaratini <159515333+paolaonzara@users.noreply.github.com>
2025-09-07 00:37:06 -0300

    Add files via upload

---
+++ AppFlins/appflins/app/+not-found.tsx
@@ -0,0 +1,35 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+import {Redirect} from 'expo-router'
+
+export default function Home(){
+   return<Redirect href='/tabs' />
+}
+
+export  function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops! Not Found' }} />
+      <View style={styles.container}>
+        <Link href="/tabs" style={styles.button}>
+          Go back to Home screen!
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
